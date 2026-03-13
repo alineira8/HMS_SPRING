ef33f5414a492bc4ada9952d80e618a221190832
Aline <alineira828@gmail.com>
2026-03-13 09:26:44 +0200

    Add AppConfig class

---
+++ src/main/java/org/example/Config/AppConfig.java
@@ -0,0 +1,20 @@
+package org.example.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Configuration
+@ComponentScan("org.example")
+public class AppConfig {
+
+    @Bean
+    public Connection getConnection() throws SQLException {
+        DataBaseConnection connection = new DataBaseConnection();
+        return connection.getConnection();
+    }
+
+}
